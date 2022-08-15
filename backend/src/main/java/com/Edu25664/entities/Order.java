cab49d28bbb3c05f63df53a6b0083bdecc4c8ef7
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-08-15 13:41:15 -0300

    backend create

---
+++ backend/src/main/java/com/Edu25664/entities/Order.java
@@ -0,0 +1,41 @@
+package com.Edu25664.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class Order implements Serializable{
+    private Long id;
+    private Instant moment;
+    private Double total;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Order(Long id, Instant moment, Double total) {
+        this.id = id;
+        this.moment = moment;
+        this.total = total;
+    }
+
+}
