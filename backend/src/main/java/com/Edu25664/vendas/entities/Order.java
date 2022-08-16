e6b33a0b7cd157e465341d021ca73a96702dda34
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-08-16 16:42:20 -0300

    seed DB

--- backend/src/main/java/com/Edu25664/vendas/entities/Order.java
+++ backend/src/main/java/com/Edu25664/vendas/entities/Order.java
@@ -2,11 +2,51 @@ package com.Edu25664.vendas.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
-public class Order implements Serializable{
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String address;
+    private Double latitude;
+    private Double longitude;
     private Instant moment;
-    private Double total;
+    private OrderStatus status;
+
+    @ManyToMany
+    @JoinTable(name = "tb_order_product", joinColumns = @JoinColumn(name = "order_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private Set<Product> products = new HashSet<>();
+
+    public Order() {
+    }
+
+    public Order(Long id, String address, Double latitude, Double longitude, Instant moment,
+            OrderStatus status, Set<Product> products) {
+        this.id = id;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.moment = moment;
+        this.status = status;
+        this.products = products;
+    }
 
     public Long getId() {
         return id;
@@ -14,6 +54,30 @@ public class Order implements Serializable{
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String setAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 
     public Instant getMoment() {
@@ -24,18 +88,41 @@ public class Order implements Serializable{
         this.moment = moment;
     }
 
-    public Double getTotal() {
-        return total;
+    public OrderStatus getOrder() {
+        return status;
     }
 
-    public void setTotal(Double total) {
-        this.total = total;
+    public void setOrder(OrderStatus status) {
+        this.status = status;
     }
 
-    public Order(Long id, Instant moment, Double total) {
-        this.id = id;
-        this.moment = moment;
-        this.total = total;
+    public Set<Product> getProduct() {
+        return products;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
 }
